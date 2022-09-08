43750a6a71d6edda8564fbd6dabfb1c8bebf93e8
JaeCodes17 <davidokaforebuka17@gmail.com>
2022-09-08 11:53:12 -0700

    Task 4Solved

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,11 @@
  *
  * Return: Always 0 (Successful)
  */
-
 int main(void)
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual
+		       	puzzle");
 	return(0);
 }
 		
