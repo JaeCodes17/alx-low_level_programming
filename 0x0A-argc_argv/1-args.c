0aeb45258a06cc5ae0d4a3350ad8d9da128adfa7
JaeCodes17 <davidokaforebuka17@gmail.com>
2022-09-30 19:00:24 -0700

    Task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,14 @@
+#include <stdio.h>
 
 /**
- * main - prints the name number of aruments passed to it
+ * main - prints the name number of arguments passed to it
  * @argc: number of arguments passed to the function
- * @argv: atgument vector of pointers to strings
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
